cdb6d1d484a0c0de632157a021f5033973b77b35
bypassOS <joegoldberg110010100@gmail.com>
2026-08-08 11:38:30 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tokio::{io::AsyncReadExt, net::{TcpListener, TcpStream}};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}};
 #[tokio::main]
 async fn main() {
     let connect = TcpListener::bind("127.0.0.1:8080").await.unwrap();
@@ -7,20 +7,25 @@ async fn main() {
 
     }
 }
-async fn copy_data(mut from: TcpStream, to:TcpStream) {
+async fn copy_data(mut from: TcpStream, mut to:TcpStream) {
     let mut buffer = [0u8; 1024];
     loop {
         let n = match from.read(&mut buffer).await  {
             Ok(0) => {
-
+                println!("The connect is closet!");
+                break;
             },
-            Ok(n) => {
-
-            }
+            Ok(n) => n,
             Err(e) => {
-
-            }
+                println!("The error of read: {e}");
+                break;
+            },
         };
+        if let Err(e) = to.write_all(&buffer[..n]).await {
+            println!("The error of write!");
+            break;
+        }
+        println!("Was sent {n} bytes");
     };
     
 }
