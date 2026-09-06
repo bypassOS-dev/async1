4c33215164718a9b171741da47436174731851e5
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-09-06 17:37:28 +0500

    Add checking of Tls handshake

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use nfq::{Queue, Verdict};
+use pnet::packet::{Packet, ipv4::Ipv4Packet, tcp::TcpPacket};
+
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let mut queue = Queue::open()?;
     queue.bind(0)?;
@@ -6,6 +8,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     loop {
         let mut msg = queue.recv()?;
         let payload = msg.get_payload();
+
+        if let Some(ip_packet) = Ipv4Packet::new(payload) {
+                if let Some(tcp_packet) = TcpPacket::new(ip_packet.payload()) {
+                let tcp_payload = tcp_packet.payload();
+                println!(
+                    "Packet: seq: {}, tcp payload lenght: {}",
+                    tcp_packet.get_sequence(),
+                    tcp_payload.len()
+                );
+                if tcp_payload.len() > 5 && tcp_payload[0] == 0x16 {
+                    println!("This look like a TLS handshake packet!");
+                }
+            };
+        };
 
         println!("Got a packet! Size: {} bytes", payload.len());
 
